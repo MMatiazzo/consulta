ad0c09fb22af4a9916b205966497e7b36ee9cd40
Lucas Renó <lucasreno9@gmail.com>
2024-07-23 02:31:53 -0300

    feat: correções owasp zap

--- src/application/server-application.ts
+++ src/application/server-application.ts
@@ -2,11 +2,28 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { RootModule } from './di/root.module';
+import helmet from 'helmet';
+import * as session from 'express-session';
 
 export class ServerApplication {
   public async run(): Promise<void> {
     const app: NestExpressApplication =
       await NestFactory.create<NestExpressApplication>(RootModule);
+    app.use(helmet.hidePoweredBy());
+    app.use(helmet.noSniff());
+
+
+    app.use(session({
+      secret: 'g11soat4',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 3600000,
+      },
+    }));
+
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3334);
   }
